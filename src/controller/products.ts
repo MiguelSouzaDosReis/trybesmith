34975859011dfcc1c4576fc5f97d70e9ca096521
Miguel_Reis <guelsouzar@gmail.com>
2022-04-11 20:33:12 -0300

    req 01 feito

--- src/controller/products.ts
+++ src/controller/products.ts
@@ -1,8 +1,8 @@
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import CRUDProducts from '../models/products';
 import connection from '../models/connection';
 
-const everthing = async (_req: any, res: Response) => {
+const everthing = async (_req: Request, res: Response) => {
   const products = await new CRUDProducts(connection).getAll();
 
   res.status(200).json(products);
